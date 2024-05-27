f8f9cc4ac863dd7422eb338aed4ea4f10f50fe34
Ferrann20A <alumno.398783@ies-azarquiel.es>
2024-05-27 20:15:29 +0200

    controllers y css

---
+++ ApiTabeAWS/Controllers/PedidosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTabeAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+
+    }
+}
